6f419a1e1743b7679203a533ec5b0b0b6d1957cf
wangyuan20 <wangyuan20@jd.com>
2018-09-28 15:42:46 +0800

    commit sqliteHandler for accessing db

--- bot1/main.py
+++ bot1/main.py
@@ -7,14 +7,16 @@
 __author__ = "WangYuan <owensnow@qq.com>"
 
 import telebot
-from bot1.models import ToDoList
 import logging
 import time
+import SqliteHandler
 
 telebot.logger.setLevel(logging.DEBUG)
 
+DB_PATH = 'D:\pythonProject\TelegramBotWeb\db.sqlite3'
 API_TOKEN = '670941114:AAHBBrx7ke3b45VYXI3dApazyBmpXyY53qE'
 bot = telebot.TeleBot(API_TOKEN)
+db = SqliteHandler.SqliteHandler(DB_PATH)
 
 
 @bot.message_handler(commands=['start'])
@@ -26,6 +28,7 @@ def send_welcome(message):
 def send_welcome(message):
     name = extract_unique_code1(message.text)
     print(name)
+    print(db.execute("select * from auth_user"))
     bot.send_message(message.chat.id, "coming ASAP...")
 
 
@@ -35,11 +38,6 @@ def send_welcome(message):
     context = extract_unique_code2(message.text)
     print(name)
     print(context)
-    tdL = ToDoList()
-    tdL.name = name
-    tdL.context = context
-    tdL.createdTime = time.time()
-    tdL.save()
     bot.send_message(message.chat.id, "had record your todoList.You can input /list to check")
 
 
@@ -54,17 +52,14 @@ def send_welcome(message):
 
 
 def extract_unique_code1(text):
-    # Extracts the unique_code from the sent /start command.
     return text.split()[1]
 
 
 def extract_unique_code2(text):
-    # Extracts the unique_code from the sent /start command.
     return text.split()[2]
 
 
 def extract_unique_code3(text):
-    # Extracts the unique_code from the sent /start command.
     return text.split()[3]
 
 
